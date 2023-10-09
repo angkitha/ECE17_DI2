0784063fd6ce584ec639ab1551bde386f86c00b1
angkitha <79730026+angkitha@users.noreply.github.com>
2023-10-09 16:39:13 -0700

    Add files via upload
    final submission

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
+using namespace std;
 
-#include "FizzBuzz.h"
-
+void fizzBuzz(int divisorOne, int divisorTwo, int currentNumber) {
+    if(currentNumber%divisorOne == 0 && currentNumber%divisorTwo == 0){
+        cout << "FizzBuzz, ";
+    }
+    else if(currentNumber%divisorOne == 0){
+        cout << "Fizz, ";
+    }
+    else if(currentNumber%divisorTwo == 0){
+        cout << "Buzz, ";
+    }
+    else{
+        cout << to_string(currentNumber) + ", ";
+    }
+}
 int main() {
-    fizzBuzz();
+    int currentDivisorOne = 3;
+    int currentDivisorTwo = 5;
+
+    int count = 2;
+    int limit = 500;
+    
+    for (int i = 1; i <= limit; i = i + count) {
+        fizzBuzz(currentDivisorOne, currentDivisorTwo, i);
+    }
 }
